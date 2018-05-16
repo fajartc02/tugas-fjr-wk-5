48763d048b11501f618572d75f501c45f13b6f76
fajartc02 <fajartc02@gmail.com>
2018-05-16 14:55:42 +0700

    submit graduates.js

--- naikAngkot.js
+++ naikAngkot.js
@@ -2,32 +2,19 @@ function naikAngkot(arrPenumpang) {
   rute = ['A', 'B', 'C', 'D', 'E', 'F'];
   //your code here
   var cargo = 2000
-  var arrAngkot = []
-  if (arrPenumpang.length > 0) {
-      for (var i = 0; i < arrPenumpang.length; i++) {
-          var objAngkot = {}
-          for (var j = 0; j < arrPenumpang[i].length; j++) {
-              if(arrPenumpang[i][j] === 'Dimitri') {
-                  objAngkot.penumpang = arrPenumpang[i][j]
-                  objAngkot.naikDari = arrPenumpang[i][1]
-                  objAngkot.tujuan = arrPenumpang[i][2]
-                  objAngkot.bayar = cargo * (rute.indexOf(objAngkot.tujuan) - rute.indexOf(objAngkot.naikDari))
-                  arrAngkot.push(objAngkot)
-              } else if (arrPenumpang[i][j] === 'Icha') {
-                  objAngkot.penumpang = arrPenumpang[i][j]
-                  objAngkot.naikDari = arrPenumpang[i][1]
-                  objAngkot.tujuan = arrPenumpang[i][2]
-                  objAngkot.bayar = cargo * (rute.indexOf(objAngkot.tujuan) - rute.indexOf(objAngkot.naikDari))
-                  arrAngkot.push(objAngkot)
-                } 
-            }
-        }
-        return arrAngkot
-    } else {
-        return arrAngkot
-    }
+  var angkotArr = []
+  for (var i = 0 ; i < arrPenumpang.length; i++) {
+    var angkot = {}
+    // for (var j = 0; j < arrPenumpang[i].length; j++) {
+        angkot.penumpang = arrPenumpang[i][0]
+        angkot.naikDari = arrPenumpang[i][1]
+        angkot.tujuan = arrPenumpang[i][2]
+        angkot.bayar = (rute.indexOf(arrPenumpang[i][2]) - rute.indexOf(arrPenumpang[i][1])) * cargo
+    // }
+    angkotArr.push(angkot)
+  }
+  console.log(angkotArr)
 }
-    // console.log(arrAngkot)
 
 
 //TEST CASE
@@ -35,4 +22,4 @@ console.log(naikAngkot([['Dimitri', 'B', 'F'], ['Icha', 'A', 'B']]));
 // [ { penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 },
 //   { penumpang: 'Icha', naikDari: 'A', tujuan: 'B', bayar: 2000 } ]
 
-console.log(naikAngkot([])); //[]
+// console.log(naikAngkot([])); //[]
