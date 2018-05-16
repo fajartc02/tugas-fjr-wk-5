dfa71c97e1a2361b9ac12dae4d1e6fd18c045efe
fajartc02 <fajartc02@gmail.com>
2018-05-16 20:44:27 +0700

    submit damageCalculation.js

--- sandbox.js
+++ sandbox.js
@@ -1,101 +1,25 @@
-/*
-{
-    class : [{name: '' , score: 0}, {name: '', score: 0}]
-}
-*/
-function graduates (students) {
-    // Code disini
-    var school = {}
-    for (var a = 0; a < students.length; a++) {
-        var nameStudents = {}
-        var studentsClass = students[a].class
-        if(school[studentsClass] === undefined) {
-            school[studentsClass] = []
-        }
-        if(students[a].score >= 75) {
-            //tampung dulu objek beserta key dan value
-            nameStudents.name = students[a].name
-            nameStudents.score = students[a].score
-            //push object kedalam array
-            school[studentsClass].push(nameStudents)
+function meleeRangedGrouping (str) {
+    //your code here
+    var arrHerro = []
+    for (var i = 0; i < 2; i++) {
+        arrHerro.push([])
+    }
+    var containStr = ''
+    for (var i = 0; i < str.length; i++) {
+        if(str[i] !== '-') {
+            containStr += str[i]
         }
     }
-    return school
+    arrHerro[1].push(containStr)
+    console.log(arrHerro)
   }
   
-  console.log(graduates([
-    {
-      name: 'Dimitri',
-      score: 90,
-      class: 'foxes'
-    },
-    {
-      name: 'Alexei',
-      score: 85,
-      class: 'wolves'
-    },
-    {
-      name: 'Sergei',
-      score: 74,
-      class: 'foxes'
-    },
-    {
-      name: 'Anastasia',
-      score: 78,
-      class: 'wolves'
-    }
-  ]));
+  // TEST CASE
   
-  // {
-  //   foxes: [
-  //     { name: 'Dimitri', score: 90 }
-  //   ],
-  //   wolves: [
-  //     { name: 'Alexei' , score: 85 },
-  //     { name: 'Anastasia', score: 78 }
-  //   ]
-  // }
+  console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
+  // [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
   
-  console.log(graduates([
-    {
-      name: 'Alexander',
-      score: 100,
-      class: 'foxes'
-    },
-    {
-      name: 'Alisa',
-      score: 76,
-      class: 'wolves'
-    },
-    {
-      name: 'Vladimir',
-      score: 92,
-      class: 'foxes'
-    },
-    {
-      name: 'Albert',
-      score: 71,
-      class: 'wolves'
-    },
-    {
-      name: 'Viktor',
-      score: 80,
-      class: 'tigers'
-    }
-  ]));
+//   console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
+//   // [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
   
-//   // {
-//   //   foxes: [
-//   //     { name: 'Alexander', score: 100 },
-//   //     { name: 'Vladimir', score: 92 }
-//   //   ],
-//   //   wolves: [
-//   //     { name: 'Alisa', score: 76 },
-//   //   ],
-//   //   tigers: [
-//   //     { name: 'Viktor', score: 80 }
-//   //   ]
-//   // }
-  
-  
-  console.log(graduates([])); //{}
+//   console.log(meleeRangedGrouping('')); // []
